55c597bd1197bffb9f4c3d3417c053a3a7ab9713
Tancredo Souza <aqueletantan@gmail.com>
2020-04-30 20:41:08 -0300

    test(client): add test

--- exercise_05/client/client.go
+++ exercise_05/client/client.go
@@ -7,8 +7,25 @@ import (
 
 func main() {
 	namingProxy := service.NamingServiceProxy{"localhost", 3999}
-	queueProxy := namingProxy.Lookup("FilaDoMal")
+	queueProxy := namingProxy.Lookup("app.Queue")
+	stackProxy := namingProxy.Lookup("app.Stack")
 
-	fmt.Println(queueProxy.GetSize())
-	fmt.Scanln()
+	test(queueProxy)
+	test(stackProxy)
+}
+
+func test(proxy service.Proxy) {
+	fmt.Println(proxy.InsertElement(5))
+	fmt.Println(proxy.InsertElement(2))
+	fmt.Println(proxy.InsertElement(4))
+	fmt.Println(proxy.InsertElement(6))
+	fmt.Println(proxy.InsertElement(1))
+
+	fmt.Println(proxy.GetSize())
+	fmt.Println(proxy.RemoveElement())
+	fmt.Println(proxy.RemoveElement())
+	fmt.Println(proxy.GetSize())
+	fmt.Println(proxy.GetFirstElement())
+
+	fmt.Println("---------------------------------")
 }
