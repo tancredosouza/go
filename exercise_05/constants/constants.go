405cf8e685c56f9f8fa7f0f9797a91682a4a1b92
Tancredo Souza <aqueletantan@gmail.com>
2020-04-30 19:30:20 -0300

    feat: add more constants to constants.go file

--- exercise_05/constants/constants.go
+++ exercise_05/constants/constants.go
@@ -1,9 +1,11 @@
 package constants
 
 const (
-	STACK_ID = 1
-	STACK_TYPE = "stack"
-	QUEUE_ID = 2
-	QUEUE_TYPE = "queue"
-	REQUEST_TYPE = 100
+	STACK_ID         = 1
+	STACK_TYPE       = "stack"
+	QUEUE_ID         = 2
+	QUEUE_TYPE       = "queue"
+	REQUEST_TYPE     = 100
+	OK_STATUS        = 200
+	NOT_FOUND_STATUS = 404
 )
