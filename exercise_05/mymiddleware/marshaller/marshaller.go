b8822ff0267edbc31678dff4026c80d68aecf233
Tancredo Souza <aqueletantan@gmail.com>
2020-05-04 16:14:45 -0300

    refactor(marshaller): replace json with msgpack

--- exercise_05/mymiddleware/marshaller/marshaller.go
+++ exercise_05/mymiddleware/marshaller/marshaller.go
@@ -1,16 +1,16 @@
 package marshaller
 
 import (
-	"../protocol"
 	"bytes"
-	"encoding/json"
+	"github.com/my/repo/mymiddleware/protocol"
+	"github.com/vmihailenco/msgpack"
 	"log"
 )
 
 type Marshaller struct{}
 
 func (t Marshaller) Marshall(data interface{}) []byte {
-	serializedData, err := json.Marshal(data)
+	serializedData, err := msgpack.Marshal(data)
 	if (err != nil) {
 		log.Fatal("Error serializing data. ", err)
 	}
@@ -20,7 +20,7 @@ func (t Marshaller) Marshall(data interface{}) []byte {
 
 func (t Marshaller) Unmarshall(b []byte) protocol.Packet {
 	var data protocol.Packet
-	err := json.Unmarshal(bytes.Trim(b, "\x00"), &data)
+	err := msgpack.Unmarshal(bytes.Trim(b, "\x00"), &data)
 
 	if (err != nil) {
 		log.Fatal("Error deserializing data. ", err)
