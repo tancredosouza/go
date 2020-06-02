8191e6aaddf138198565a722ba51f2a40411b5bb
Tancredo Souza <aqueletantan@gmail.com>
2020-06-02 14:06:57 -0300

    feat(buffers): add Topics buffer

--- tmq/buffers/buffers.go
+++ tmq/buffers/buffers.go
@@ -3,8 +3,10 @@ package buffers
 import "log"
 
 var IncomingMessages chan []byte
+var Topics map[string] chan []byte
 
 func Initialize() {
 	IncomingMessages = make(chan []byte, 100)
+	Topics = make(map[string] chan []byte)
 	log.Println("Initialized message buffers..")
 }
