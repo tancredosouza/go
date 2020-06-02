f2fefa5eec420ba73e199a89533e2cca125fb9c1
Tancredo Souza <aqueletantan@gmail.com>
2020-06-02 10:35:47 -0300

    refactor(srh): create transmission.go and move Send() and Receive()

--- tmq/infrastructure/serverRequestHandler.go
+++ tmq/infrastructure/serverRequestHandler.go
@@ -71,17 +71,6 @@ func handleNewConnection(conn net.Conn) {
 	}
 }
 
-func Receive(conn net.Conn) ([]byte, error) {
-	msg := make([]byte, 512)
-	_, err := conn.Read(msg)
-	return msg, err
-}
-
-func Send(msg []byte, conn net.Conn) error {
-	_, err := conn.Write(msg)
-	return err
-}
-
 func CloseConnection(conn net.Conn) {
 	err := conn.Close()
 	if (err != nil) {
