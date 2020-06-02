3075dfd191291a5f1ebafc4449b540cd99d56101
Tancredo Souza <aqueletantan@gmail.com>
2020-06-02 12:02:37 -0300

    feat(component): self-assigns and id

--- tmq/component/component.go
+++ tmq/component/component.go
@@ -1,19 +1,38 @@
 package component
 
-import "../marshaller"
-import "../infrastructure"
+import (
+	"../infrastructure"
+	"../marshaller"
+	"../protocol"
+	"math/rand"
+)
 
 type Component struct{
+	id string
 	crh infrastructure.ClientRequestHandler
 }
 
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
 func (c *Component) Dial(serverHost string, serverPort int) {
+	c.id = RandStringBytes(4)
+
 	c.crh = infrastructure.ClientRequestHandler{
 		serverHost,
 		serverPort}
+
+	c.Publish(protocol.Packet{"register", []interface{}{c.id} })
 }
 
-func (c *Component) Publish(msgToSend string) {
+func (c *Component) Publish(msgToSend protocol.Packet) {
 	m := marshaller.Marshaller{}
 
 	bytesToSend := m.Marshall(msgToSend)
