dfabbf4f38f88b785c0f98fd405f2921fa5a7f27
Tancredo Souza <aqueletantan@gmail.com>
2020-06-02 14:07:51 -0300

    feat(transmission): send message size before-hand

--- tmq/infrastructure/transmission.go
+++ tmq/infrastructure/transmission.go
@@ -1,14 +1,32 @@
 package infrastructure
 
-import "net"
+import (
+	"encoding/binary"
+	"net"
+)
 
 func Receive(conn net.Conn) ([]byte, error) {
-	msg := make([]byte, 512)
-	_, err := conn.Read(msg)
+	msgSize := make([]byte, 4)
+	n, err := conn.Read(msgSize)
+
+	if (n == 0 || err != nil) {
+		return nil, err
+	}
+
+	size := binary.LittleEndian.Uint32(msgSize)
+
+	msg := make([]byte, size)
+	_, err = conn.Read(msg)
 	return msg, err
 }
 
 func Send(msg []byte, conn net.Conn) error {
+	msgSize := make([]byte, 4)
+	l := uint32(len(msg))
+
+	binary.LittleEndian.PutUint32(msgSize, l)
+	conn.Write(msgSize)
+
 	_, err := conn.Write(msg)
 	return err
 }
