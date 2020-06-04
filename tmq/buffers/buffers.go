3e2c3ec72bbc7c31dc6d52abc27a02162e0084f3
Tancredo Souza <aqueletantan@gmail.com>
2020-06-03 23:34:28 -0300

    feat(buffers): add Notify channel

--- tmq/buffers/buffers.go
+++ tmq/buffers/buffers.go
@@ -3,10 +3,14 @@ package buffers
 import "log"
 
 var IncomingMessages chan []byte
-var Topics map[string] chan []byte
+var Topics map[string] chan float64
+var Subscribers map[string] []string
+var Notify chan string
 
 func Initialize() {
 	IncomingMessages = make(chan []byte, 100)
-	Topics = make(map[string] chan []byte)
+	Topics = make(map[string] chan float64)
+	Notify = make(chan string, 100)
+	Subscribers = make(map[string] []string)
 	log.Println("Initialized message buffers..")
 }
