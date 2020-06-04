b1a7bc65384ed8da38ab6474b8efe304ce814750
Tancredo Souza <aqueletantan@gmail.com>
2020-06-03 23:39:56 -0300

    feat(tmq): initialize notification engine

--- tmq/tmq.go
+++ tmq/tmq.go
@@ -1,18 +1,51 @@
 package main
 
-import "./buffers"
-import "./infrastructure"
-import "./orchestrator"
+import (
+	"./buffers"
+	"./operator"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
 	InitializeMiddleware()
 }
 
 func InitializeMiddleware() {
-	orchestrator.Initialize()
+	/*
+	fmt.Println("Go MySQL Tutorial")
 
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/test")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+
+	// perform a db.Query insert
+	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
+
+	// /* ------------------------- */
+
+	operator.Initialize()
 	buffers.Initialize()
 
-	srh := infrastructure.ServerRequestHandler{"localhost", 3993}
-	srh.Initialize()
+	ne := operator.NotificationEngine{ServerHost: "localhost", ServerPort: 3993}
+	go ne.Initialize()
+
+	fmt.Scanln()
 }
