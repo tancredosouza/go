6d077b600706ccad37327f915a917eded4aac378
Tancredo Souza <aqueletantan@gmail.com>
2020-06-03 23:34:50 -0300

    feat(operator): implement publish/subscribe functions

--- tmq/operator/operator.go
+++ tmq/operator/operator.go
@@ -3,6 +3,7 @@ package operator
 import (
 	"../buffers"
 	"../marshaller"
+	"../protocol"
 	"fmt"
 	"log"
 )
@@ -27,12 +28,30 @@ func operate(msg []byte) {
 	switch packet.Operation {
 	case "create":
 		createTopic(packet.Params[1].(string))
+	case "publish":
+		publish(packet)
+	case "subscribe":
+		subscribe(packet)
 	}
 }
 
 func createTopic(name string) {
 	if _, found := buffers.Topics[name]; !found {
-		buffers.Topics[name] = make(chan []byte, 100)
+		buffers.Topics[name] = make(chan float64, 100)
+		buffers.Notify <- name
 		log.Println(fmt.Sprintf("Created topic with name %s!", name))
 	}
+}
+
+func publish(p protocol.Packet) {
+	topicName := p.Params[1].(string)
+	message := p.Params[2].(float64)
+
+	buffers.Topics[topicName] <- message
+}
+
+func subscribe(p protocol.Packet) {
+	connId := p.Params[0].(string)
+	topicName := p.Params[1].(string)
+	buffers.Subscribers[topicName] = append(buffers.Subscribers[topicName], connId)
 }
