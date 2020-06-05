1330e535df8ca7e27f33715071403dd19fbe5f21
Tancredo Souza <aqueletantan@gmail.com>
2020-06-05 14:10:12 -0300

    fix(transmission): add nil check

--- tmq/infrastructure/transmission.go
+++ tmq/infrastructure/transmission.go
@@ -2,22 +2,27 @@ package infrastructure
 
 import (
 	"encoding/binary"
+	"errors"
 	"net"
 )
 
 func Receive(conn net.Conn) ([]byte, error) {
 	msgSize := make([]byte, 4)
-	n, err := conn.Read(msgSize)
+	if (conn != nil) {
+		n, err := conn.Read(msgSize)
 
-	if (n == 0 || err != nil) {
-		return nil, err
+		if (n == 0 || err != nil) {
+			return nil, err
+		}
+
+		size := binary.LittleEndian.Uint32(msgSize)
+
+		msg := make([]byte, size)
+		_, err = conn.Read(msg)
+		return msg, err
+	} else {
+		return nil, errors.New("nil connection")
 	}
-
-	size := binary.LittleEndian.Uint32(msgSize)
-
-	msg := make([]byte, size)
-	_, err = conn.Read(msg)
-	return msg, err
 }
 
 func Send(msg []byte, conn net.Conn) error {
@@ -26,12 +31,16 @@ func Send(msg []byte, conn net.Conn) error {
 
 	binary.LittleEndian.PutUint32(msgSize, l)
 
-	_, err := conn.Write(msgSize)
+	if (conn != nil) {
+		_, err := conn.Write(msgSize)
 
-	if (err != nil) {
-		return err
+		if (err != nil) {
+			return err
+		}
+	} else {
+		return errors.New("nil connection")
 	}
 
-	_, err = conn.Write(msg)
+	_, err := conn.Write(msg)
 	return err
 }
