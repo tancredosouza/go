038707f3744f70d4f580bda819ae3d617d08e4bc
Tancredo Souza <aqueletantan@gmail.com>
2020-06-07 11:45:33 -0300

    feat(heater): add rabbitmq implementation

--- tmq/heater/heater.go
+++ tmq/heater/heater.go
@@ -1,16 +1,62 @@
 package main
 
 import (
-	"../component"
-	"fmt"
+	"github.com/streadway/amqp"
 	"log"
 )
-
-func main() {
-	c := component.Component{Key: 101}
+/*
+var tmqComponent = InitializeTmqComponent()
+func InitializeTmqComponent() component.Component {
+	var c = component.Component{Key: 101}
 	c.TmqConnect("localhost", 3993)
 
-	for {
-		log.Println(fmt.Sprintf("Temperatura configurada para %f", <- c.SubscriptionMessages))
-	}
+	return c
+}
+
+func TMQ_ListenForChangesInTemperature(room string) {
+	go func() {
+		for {
+			fmt.Println("Temp received = ", <- tmqComponent.SubscriptionMessages)
+		}
+	}()
+
+	fmt.Scanln()
+}
+*/
+
+var rabbitMqChannel = RabbitMQ_Initialize()
+func RabbitMQ_Initialize() (*amqp.Channel) {
+	conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	ch, _ := conn.Channel()
+
+	return ch
+}
+
+func RabbitMQ_ListenForTemperatureChange(room string) {
+	forever := make(chan bool)
+
+	msgs, _ := rabbitMqChannel.Consume(
+		room, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	go func() {
+		for {
+			for d := range msgs {
+				log.Printf("Received a message: %s", d.Body)
+			}
+		}
+	}()
+
+	<- forever
+}
+
+func main() {
+	// TMQ_ListenForChangesInTemperature("Sala")
+	RabbitMQ_ListenForTemperatureChange("Sala")
 }
