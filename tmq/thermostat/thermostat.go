7ab76c8248ebe1af15641a5686481530bdb71032
Tancredo Souza <aqueletantan@gmail.com>
2020-06-07 11:45:52 -0300

    feat(thermostat): add rabbitmq implementation

--- tmq/thermostat/thermostat.go
+++ tmq/thermostat/thermostat.go
@@ -1,32 +1,61 @@
 package main
 
 import (
-	"../component"
-	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+	"strconv"
+	"time"
 )
 
-
-func main() {
-	for i:=1;i<2;i++ {
-		go operate(i)
-	}
-
-	fmt.Scanln()
-}
-
-func operate(i int) {
-	var c = component.Component{Key: i}
+/*
+var tmqComponent = InitializeTmqComponent()
+func InitializeTmqComponent() component.Component {
+	var c = component.Component{Key: 9999}
 	c.TmqConnect("localhost", 3993)
 
-	c.CreateTopic("Sala")
-	c.CreateTopic("Quarto 1")
-	c.CreateTopic("Quarto 2")
-	c.CreateTopic("Cozinha")
+	return c
+}
 
-	//time.Sleep(5*time.Second)
-	c.Publish("Sala", 25)
-	for i:=0;i<50;i++{
-		c.Publish("Cozinha", 18)
-		c.Publish("Sala", 23)
+func TMQ_ChangeRoomTemperature(room string, desiredTemperature int) {
+	tmqComponent.Publish(room, desiredTemperature)
+}
+*/
+
+var rabbitMqChannel = RabbitMQ_Initialize()
+func RabbitMQ_Initialize() (*amqp.Channel) {
+	conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	ch, _ := conn.Channel()
+
+	return ch
+}
+
+func RabbitMQ_ChangeRoomTemperature(room string, desiredTemperature int) {
+	q, _ := rabbitMqChannel.QueueDeclare(
+		room, // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+
+	body := strconv.Itoa(desiredTemperature)
+	_ = rabbitMqChannel.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing {
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+}
+
+func main() {
+	for i := 0; i < 10000; i++ {
+		log.Println("Sending temperature...")
+		time.Sleep(time.Second)
+		//TMQ_ChangeRoomTemperature("Sala", 19)
+		RabbitMQ_ChangeRoomTemperature("Sala", 19)
 	}
 }
